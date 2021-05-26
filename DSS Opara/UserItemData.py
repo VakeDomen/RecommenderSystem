713b8f3b330460e38a897dd832c657fd32ff3e4b
Domen Vake <vake.domen94@gmail.com>
2021-05-26 15:53:36 +0200

    druga

--- DSS Opara/UserItemData.py
+++ DSS Opara/UserItemData.py
@@ -30,8 +30,8 @@ class UserItemData(object):
 
 
 if __name__ == "__main__":
-    uim = UserItemData('data/user_ratedmovies.dat')
+    uim = UserItemData('../data/user_ratedmovies.dat')
     print(uim.nratings())
 
-    uim = UserItemData('data/user_ratedmovies.dat', start_date='12.1.2007', end_date='16.2.2008', min_ratings=100)
+    uim = UserItemData('../data/user_ratedmovies.dat', start_date='12.1.2007', end_date='16.2.2008', min_ratings=100)
     print(uim.nratings())
