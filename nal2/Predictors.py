fafb36bf2b61ddc5e73b540418ec1efdd2100a64
Domen Vake <vake.domen94@gmail.com>
2021-05-27 22:50:06 +0200

    slopes?

--- nal2/Predictors.py
+++ nal2/Predictors.py
@@ -1,4 +1,6 @@
 import numpy as np
+from numpy.core.numeric import NaN
+import math
 import pandas as pd
 from numpy import dot
 from numpy.linalg import norm
@@ -85,7 +87,6 @@ class ItemBasedPredictor:
         return similarity
         
     def similarItems(self, movie_id, n=5):
-        all_movie_ids = sorted(self.rating_table_with_nan.columns)
         # dict to store the generated ratings or the user
         movie_ratings = {}
         # all similarities with other movies
@@ -181,3 +182,109 @@ class MovieSimilarityBasedPredictor:
             return 0
         # return similarity
         return similarity
+
+
+class SlopeOnePredictor:
+    def __init__(self, min_values=0, treshold=0):
+        self.min_values     = min_values
+        self.treshold       = treshold
+
+    def fit(self, user_data):
+        # save data of ratings
+        self.data = user_data.data
+        # save the pivot table (userID:movieID => value in tabe is the rating of 
+        # the movie by the user | or NaN is no rating has been given)
+        self.rating_table = pd.pivot_table(self.data, values="rating", index=["userID"], columns=['movieID'])
+
+    def slopeOne(self, id, user_id):
+        # ratings of the user, we are using to predict ratings
+        ratings = self.rating_table.loc[[id, user_id]]
+        columns = ratings.columns
+        # nase
+        r = ratings.values[0]
+        # njegove
+        ur = ratings.values[1]
+
+        cols_to_count = 0
+        sum = 0
+
+        for i in range(len(columns)):
+            if not math.isnan(r[i]) and not math.isnan(ur[i]):
+                cols_to_count = cols_to_count + 1
+                sum = sum + ur[i] - r[i]
+        if cols_to_count > 0:
+            sum = sum / cols_to_count
+
+        for i in range(len(columns)):
+            if math.isnan(r[i]) and not math.isnan(ur[i]):
+                r[i] = ur[i] + sum
+                print('Predicted ' + str(i) + ' -> ' + str(r[i]))
+                ratings.at[id, i] = r[i]
+                self.rating_table.at[id, i] = r[i]
+
+
+        self.rating_table.loc[id] = ratings.loc[id]
+
+    def predict(self, user_id):
+        # get all rating of the user
+        user_ratings = self.rating_table.loc[user_id]
+        # find ids for all other users so we can find the most similar users
+        all_user_ids = list(self.data['userID'].unique())
+        all_movie_ids = list(self.data['movieID'].unique())
+        user_rated_movies = list(user_ratings.dropna().keys().to_numpy())
+        
+        # init dist for user similarities
+        similarities = {}
+        # for all users save the similatity into dict
+        for id in all_user_ids:
+            similarities[id] = self.similarity(user_id, id, all_user_ids)
+        # sort dict to most similar users on start of the dict
+        similarities = dict(sorted(similarities.items(), key=lambda item: item[1], reverse=True))
+        
+        # movie ids that need to be rated
+        movies_to_rate = list(set(all_movie_ids) - set(user_rated_movies))
+        # in order fill the unrated movies untill all are filled
+        for u_id, val in similarities.items():
+            if u_id == user_id:
+                continue
+            # update user ratings with completed predictions
+            self.slopeOne(user_id, u_id)
+            # updte movies, that still need to be rated
+            movies_to_rate = list(set(all_movie_ids) - set(user_rated_movies))
+            # if all movies are rated stop rating
+            # break
+            
+            if len(movies_to_rate) == 0:
+                break
+        
+        
+        movie_ratings = {}
+        dick = dict(self.rating_table.loc[user_id])
+        movie_ratings = dict(sorted(dick.items(), key=lambda item: item[1], reverse=True))
+
+        
+        # dict to store the generated ratings or the user (will be returened at the end)
+        # for index, id in enumerate(all_movie_ids):
+        #     similarities_of_the_movie = self.adj_mat[id]
+        #     similarities_to_user_rated_movies = similarities_of_the_movie[user_rated_movies]
+        #     max_similarity = similarities_to_user_rated_movies.max()
+        #     index_max_similarity = similarities_to_user_rated_movies.idxmax()
+        #     movie_ratings[id] = max_similarity * user_ratings[index_max_similarity]
+
+        return movie_ratings
+
+    def similarity(self, user1_id, user2_id, all_user_ids):
+        # # get user 1 and user 2 ratings
+        ratings_nan = self.rating_table.loc[[user1_id, user2_id]]
+        ratings = ratings_nan.dropna(axis=1)
+        # create temporary table of the ratings
+        # if not enough ratings return simmilarity as 0
+        if len(ratings.columns) <= self.min_values:
+            return 0
+        # calc similarity
+        similarity = 1 - spatial.distance.cosine(ratings.iloc[0], ratings.iloc[1])
+        # if not similar enough, return 0
+        if similarity < self.treshold:
+            return 0
+        return similarity
+        
--- nal2/naloga_2.py
+++ nal2/naloga_2.py
@@ -136,7 +136,7 @@ In many domains, neural networks outperform traditional methods and it appears t
 
 from imports.Data import UserItemData, MovieData
 from imports.Recommender import Recommender, RecommenderTopSimilar
-from Predictors import ItemBasedPredictor, MovieSimilarityBasedPredictor
+from Predictors import ItemBasedPredictor, MovieSimilarityBasedPredictor, SlopeOnePredictor
 
 def itemBasedPredictor():
     print("Runing item based predictor...")
@@ -179,7 +179,38 @@ def mostSimilarToMovie():
     for idmovie, val in rec_items:
         print("Movie: {}, score: {}".format(md.get_title(idmovie), val))
 
+def listMoviesforMe():
+    print("Runing item based predictor...")
+    md = MovieData('../data/movies.dat')
+    uim = UserItemData('../data/user_ratedmovies.dat', min_ratings=1000)
+    rp = ItemBasedPredictor()
+    rec = Recommender(rp)
+    rec.fit(uim)
+    # print(uim.movies)
+    print("Similarity between the movies 'Men in black'(1580) and 'Ghostbusters'(2716): ", rp.similarity(1580, 2716))
+    print("Similarity between the movies 'Men in black'(1580) and 'Schindler's List'(527): ", rp.similarity(1580, 527))
+    print("Similarity between the movies 'Men in black'(1580) and 'Independence day'(780): ", rp.similarity(1580, 780))
+
+    print("Predictions for 78: ")
+    rec_items = rec.recommend(666, n=10, rec_seen=False)
+    for idmovie, val in rec_items:
+        print("Movie: {}, score: {}".format(md.get_title(idmovie), val))
+
+def slopeOnePredictor():
+    md = MovieData('../data/movies.dat') 
+    uim = UserItemData('../data/user_ratedmovies.dat', min_ratings=1000) 
+    rp = SlopeOnePredictor() 
+    rec = Recommender(rp) 
+    rec.fit(uim)
+
+    print("Predictions for 78: ") 
+    rec_items = rec.recommend(78, n=15, rec_seen=False) 
+    for idmovie, val in rec_items:
+        print("Movie: {}, score: {}".format(md.get_title(idmovie), val))
+
 if __name__ == "__main__":
     # itemBasedPredictor()
     # movieSimilarityPrediction()
-    mostSimilarToMovie()
+    # mostSimilarToMovie()
+    # listMoviesforMe()
+    slopeOnePredictor()
