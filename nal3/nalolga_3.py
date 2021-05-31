7a6102539e58266af34d4e524b28f88187bb255d
Žiga Holc <ziga.holc2341@gmail.com>
2021-05-31 19:49:44 +0200

    output

--- nal3/nalolga_3.py
+++ nal3/nalolga_3.py
@@ -4,7 +4,7 @@ from imports.Predictors import AveragePredictor, RandomPredictor, SlopeOnePredic
 
 
 if __name__ == "__main__":
-    print("Running evaluator...")
+    print("\nRunning SlopeOnePredictor...")
     md = MovieData('../data/movies.dat')
     uid = UserItemData('../data/user_ratedmovies.dat', min_ratings=1000)
     test_set = uid.getTestSetData(2053)
@@ -20,3 +20,76 @@ if __name__ == "__main__":
     print('recall: \t' + str(recall))
     print('f1:    \t\t' + str(f1))
 
+
+
+    print("\nRunning AveragePredictor...")
+    md = MovieData('../data/movies.dat')
+    uid = UserItemData('../data/user_ratedmovies.dat', min_ratings=1000)
+    test_set = uid.getTestSetData(2053)
+    rp = AveragePredictor()
+    rec = Recommender(rp)
+    rec.fit(uid)
+    
+    mae, rmse, precision, recall, f1 = rec.evaluate(2053, test_set)
+    print("------- Evaluation results: -------")
+    print('MAE:   \t\t' + str(mae))
+    print('RMSE:   \t' + str(rmse))
+    print('precision: \t' + str(precision))
+    print('recall: \t' + str(recall))
+    print('f1:    \t\t' + str(f1))
+
+
+
+    print("\nRunning RandomPredictor...")
+    md = MovieData('../data/movies.dat')
+    uid = UserItemData('../data/user_ratedmovies.dat', min_ratings=1000)
+    test_set = uid.getTestSetData(2053)
+    rp = RandomPredictor(1, 5)
+    rec = Recommender(rp)
+    rec.fit(uid)
+    
+    mae, rmse, precision, recall, f1 = rec.evaluate(2053, test_set)
+    print("------- Evaluation results: -------")
+    print('MAE:   \t\t' + str(mae))
+    print('RMSE:   \t' + str(rmse))
+    print('precision: \t' + str(precision))
+    print('recall: \t' + str(recall))
+    print('f1:    \t\t' + str(f1))
+
+
+
+
+    print("\nRunning ItemBasedPredictor...")
+    md = MovieData('../data/movies.dat')
+    uid = UserItemData('../data/user_ratedmovies.dat', min_ratings=1000)
+    test_set = uid.getTestSetData(2053)
+    rp = ItemBasedPredictor()
+    rec = Recommender(rp)
+    rec.fit(uid)
+    
+    mae, rmse, precision, recall, f1 = rec.evaluate(2053, test_set)
+    print("------- Evaluation results: -------")
+    print('MAE:   \t\t' + str(mae))
+    print('RMSE:   \t' + str(rmse))
+    print('precision: \t' + str(precision))
+    print('recall: \t' + str(recall))
+    print('f1:    \t\t' + str(f1))
+
+
+
+
+    print("\nRunning ViewsPredictor...")
+    md = MovieData('../data/movies.dat')
+    uid = UserItemData('../data/user_ratedmovies.dat', min_ratings=1000)
+    test_set = uid.getTestSetData(2053)
+    rp = ViewsPredictor()
+    rec = Recommender(rp)
+    rec.fit(uid)
+    
+    mae, rmse, precision, recall, f1 = rec.evaluate(2053, test_set)
+    print("------- Evaluation results: -------")
+    print('MAE:   \t\t' + str(mae))
+    print('RMSE:   \t' + str(rmse))
+    print('precision: \t' + str(precision))
+    print('recall: \t' + str(recall))
+    print('f1:    \t\t' + str(f1))
